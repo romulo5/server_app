ef915a90e3597275d35736952dd4ca6cf113e7b0
romulo <romulofloresta@gmail.com>
2016-01-31 15:37:21 -0200

    ex2py

--- first.py
+++ first.py
@@ -1,14 +1,17 @@
 from flask import Flask
-
-import pyex
+import ex2py
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def first_json_test():
-    return pyex.response
+def api_response():
+    return ex2py.convert_xls2py()
     
+@app.route('/update')
+def update_xls():
+    return
+
 if __name__ == '__main__':
     app.run(debug=True)
 
