5f632d97b9569173bf258b12c1ceeecf0085e23b
romulo5 <romulofloresta@gmail.com>
2016-05-01 02:44:27 +0000

    switch to sql database

---
+++ resources.py
@@ -0,0 +1,14 @@
+
+#Flask Secret Key
+
+SECRET_KEY = "umgrandesegredoparaahumanidade"
+SQL_CMD = 'mysql -uromulofloresta -h romulofloresta.mysql.pythonanywhere-services.com -p#fortuna'
+DB_USER = 'romulofloresta'
+DB_PASSWD = '#fortuna'
+DB_NAME = 'romulofloresta$datadb'
+DB_HOST = 'romulofloresta.mysql.pythonanywhere-services.com'
+SQL_DROP = 'mysqladmin -uromulo -pfortuna drop'
+SQL_CREATE = 'mysqladmin -uromulo -pfortuna create'
+VERSION_FILE = '/home/romulofloresta/server_app/version.json'
+JSON_RESPONSE_FILE='/home/romulofloresta/server_app/response.json'
+UPLOAD_FOLDER = '/home/romulofloresta/server_app/sql'
